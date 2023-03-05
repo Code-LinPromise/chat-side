454c4428b4b775dc8830eff3a5458380735dab64
林鹏涛 <326661445@qq.com>
2023-03-05 17:55:49 +0800

    完成基本页面

--- src/pages/Reply/index.tsx
+++ src/pages/Reply/index.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import  s from "./style.module.scss"
 import UserQuestion from "../../components/UserQuestion";
 import AiReply from "../../components/AiReply";
+import ChatInput from "../../components/ChatInput";
 const Reply = () => {
     return (
         <div className={s.wrapper}>
-            <UserQuestion/>
-            <AiReply/>
+            <div className={s.font}>
+                <UserQuestion/>
+                <AiReply/>
+                <ChatInput/>
+            </div>
         </div>
     );
 };
